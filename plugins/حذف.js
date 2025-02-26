8397657de0ec001eb2806f47087ebb64c4b16a26
hussain2202 <hess6050@gmail.com>
2025-02-26 23:19:21 +0200

    Update حذف.js

--- plugins/حذف.js
+++ plugins/حذف.js
@@ -1,12 +1,32 @@
+/* Creditos a https://github.com/FG98F */
 
-let handler = async (m, { conn, text }) => {
-  let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
-if (room == undefined) return conn.reply(m.chat,`فين الرساله الي عايز تمسحها!`, m)
-delete conn.game[room.id]
-await conn.reply(m.chat, `تم الحذف`, m)
-}
-handler.help = ['delttt']
-handler.tags = ['game']
-handler.command = ['حذففف', 'delttt', 'delxo']
-
+let handler = async (m, { conn, usedPrefix, command }) => {	
+if (!m.quoted) throw `*❮ ❗┇يجب ان تضع ريبلاي للرسالة الذي تريد حذفها❯*`
+try {
+let delet = m.message.extendedTextMessage.contextInfo.participant
+let bang = m.message.extendedTextMessage.contextInfo.stanzaId
+return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+} catch {
+return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
+}}
+handler.help = ['del', 'delete']
+handler.tags = ['group']
+handler.command = /^حذف|احذف(ete)?$/i
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
 export default handler
+
+/*let handler = function (m) {
+if (!m.quoted) throw false
+let { chat, fromMe, isBaileys } = m.quoted
+if (!fromMe) throw false
+if (!isBaileys) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚂𝙴 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙽𝙾 𝙵𝚄𝙴 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙿𝙾𝚁 𝙼𝙸, 𝙽𝙾 𝙻𝙾 𝙿𝚄𝙴𝙳𝙾 𝙴𝙻𝙸𝙼𝙸𝙽𝙰𝚁*'
+conn.sendMessage(chat, { delete: m.quoted.vM.key })
+}
+handler.help = ['del', 'delete']
+handler.tags = ['tools']
+handler.command = /^del(ete)?$/i
+handler.group = true
+handler.admin = true
+export default handler*/
